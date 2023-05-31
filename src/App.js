b8f9a9cb5b069a1fbdcb56eeda4e52e09f0f8e0c
Jatin Lalwani <112273774+Jatin742@users.noreply.github.com>
2023-05-31 22:30:24 +0530

    Application Built

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar';
 import News from './Components/News';
 import{BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
+const dotenv=require("dotenv").config();
 
 const App=()=>{
   const pageSize=5;
